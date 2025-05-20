5d5b27d4b25c4fa2b9b538a0cf3fa0c56561afca
João Vitor Ferreira Pereira <joaovfp1911@gmail.com>
2025-05-20 20:23:15 -0300

    feat: criado tela de cadastro de usuário

---
+++ src/main/java/usjt/atividade/views/authentication/SignUp/SignUpView.java
@@ -0,0 +1,34 @@
+package usjt.atividade.views.authentication.SignUp;
+
+import usjt.atividade.views.authentication.AuthView;
+import usjt.atividade.views.authentication.Welcolme.WelcomePanel;
+
+import java.awt.*;
+
+public class SignUpView extends AuthView {
+
+    private SignUpPanel signUpPanel;
+    private WelcomePanel welcomePanel;
+
+    public SignUpView() {
+        super("Sign Up");
+        render();
+    }
+
+    @Override
+    protected void initComponents() {
+        signUpPanel = new SignUpPanel();
+        welcomePanel = new WelcomePanel("Junte-se a nós!", "ui-ux-signup.png");
+    }
+
+    @Override
+    protected void layoutComponents() {
+        leftPanel.add(welcomePanel, BorderLayout.CENTER);
+        rightPanel.add(signUpPanel, BorderLayout.CENTER);
+    }
+
+    @Override
+    protected void addListeners() {
+    }
+
+}
