9df0e1b43ef2ee9d535c4e63c7e57f60c2e7e597
João Vitor Ferreira Pereira <joaovfp1911@gmail.com>
2025-05-22 01:28:42 -0300

    feat: criado factory de components e definido padroes de estilo reutilizáveis

---
+++ src/main/java/usjt/atividade/views/authentication/utils/ComponentFactory.java
@@ -0,0 +1,52 @@
+package usjt.atividade.views.authentication.utils;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ComponentFactory {
+    public static JLabel createLabel(String text, Font font, Color color, int alignment) {
+        JLabel label = new JLabel(text);
+        label.setFont(font);
+        label.setForeground(color);
+        label.setHorizontalAlignment(alignment);
+        return label;
+    }
+
+    public static RoundedButton createRoundedButton(String text, Font font, Color bgColor, Color textColor) {
+        RoundedButton btn = new RoundedButton();
+        btn.setText(text);
+        btn.setFont(font);
+        btn.setForeground(textColor);
+        btn.setBackground(bgColor);
+        btn.setBorderColor(bgColor);
+        btn.setColorPressed(bgColor.darker());
+        btn.setColorRollover(bgColor.brighter());
+        return btn;
+    }
+
+    public static CustomTextField createCustomTextField(String placeholder, Color textColor, Color borderColor) {
+        CustomTextField field = new CustomTextField();
+        field.setPlaceholder(placeholder);
+        field.setPlaceholderColor(UIStyle.PLACEHOLDER_COLOR);
+        field.setTextColor(textColor);
+        field.setBorderColor(borderColor);
+        return field;
+    }
+
+    public static CustomPasswordField createCustomPasswordField(String placeholder, Color textColor, Color borderColor) {
+        CustomPasswordField field = new CustomPasswordField();
+        field.setPlaceholder(placeholder);
+        field.setPlaceholderColor(UIStyle.PLACEHOLDER_COLOR);
+        field.setTextColor(textColor);
+        field.setBorderColor(borderColor);
+        return field;
+    }
+
+    public static JLabel createLinkLabel(String text, Color textColor, Font font) {
+        JLabel label = new JLabel("<html><u>" + text + "</u></html>");
+        label.setForeground(textColor);
+        label.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        label.setFont(font);
+        return label;
+    }
+}
--- src/main/java/usjt/atividade/views/authentication/utils/UIStyle.java
+++ src/main/java/usjt/atividade/views/authentication/utils/UIStyle.java
@@ -1,10 +1,14 @@
 package usjt.atividade.views.authentication.utils;
 
-import javax.swing.*;
 import java.awt.*;
 
 public class UIStyle {
 
     public static final Color BG_AUTH_COLOR = new Color(0, 33, 66);
-
+    public static final Dimension AUTH_DIMENSION = new Dimension(500, 700);
+    public static final Color PLACEHOLDER_COLOR = Color.GRAY;
+    public static final Color AUTH_TEXT_COLOR = Color.WHITE;
+    public static final Color AUTH_ACCENT_COLOR = new Color(0, 140, 160);
+    public static final Font AUTH_TITLE_FONT = new Font("Arial", Font.BOLD, 24);
+    public static final Font AUTH_TEXT_FONT = new Font("Segoe UI", Font.PLAIN, 14);
 }
