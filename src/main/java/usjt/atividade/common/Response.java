aa0e7e8d266479ff5a3e0a056b00248214741a77
João Vitor Ferreira Pereira <joaovfp1911@gmail.com>
2025-05-24 16:45:00 -0300

    feat: criado objetos para identificar status das respostas das "requisições"

---
+++ src/main/java/usjt/atividade/common/Response.java
@@ -0,0 +1,45 @@
+package usjt.atividade.common;
+
+public class Response <T>{
+
+    private final boolean success;
+    private final StatusCode statusCode;
+    private final String message;
+    private final T data;
+
+    private Response(boolean success, StatusCode statusCode, String message, T data) {
+        this.success = success;
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> Response<T> ok(T data) {
+        return new Response<>(true, StatusCode.SUCCESS, null, data);
+    }
+
+    public static <T> Response<T> ok(String message, T data) {
+        return new Response<>(true, StatusCode.SUCCESS, message, data);
+    }
+
+    public static <T> Response<T> fail(StatusCode code, String message) {
+        return new Response<>(false, code, message, null);
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public StatusCode getStatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+}
---
+++ src/main/java/usjt/atividade/common/StatusCode.java
@@ -0,0 +1,26 @@
+package usjt.atividade.common;
+
+public enum StatusCode {
+    SUCCESS(200, "SUCCESS"),
+    CREATED(201, "CREATED"),
+    NOT_FOUND(404, "NOT_FOUND"),
+    VALIDATION_ERROR(422, "VALIDATION_ERROR"),
+    INTERNAL_ERROR(500, "INTERNAL_ERROR"),
+    BAD_REQUEST(400, "BAD_REQUEST");
+
+    private final int code;
+    private final String status;
+
+    StatusCode(int code, String status) {
+        this.code = code;
+        this.status = status;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
